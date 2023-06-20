fae68aa37ba758aafdf06166a451ef8e9e88a41e
volgarine <volgarine@gmail.com>
2023-06-20 16:20:53 -0600

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,39 @@
+package volgarine;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String []args) throws IOException{
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Converter from:");
+        System.out.println("(1) Canada \n(2) USA \n(3) Euro \n(4) Brazil");
+
+        String[] currency = {"CAD", "USD", "EUR", "BRL"};
+        int currencyFrom = scanner.nextInt();
+        System.out.println("Convert from " + currency[currencyFrom-1] + " to: ");
+
+        System.out.println("(1) Canada \n(2) USA \n(3) Euro \n(4) Brazil");
+
+        int currencyTo = scanner.nextInt();
+
+        System.out.println("Amount: ");
+        double amount = scanner.nextDouble();
+
+        System.out.println("Convert "+ currency[currencyFrom-1] + " to "+ currency[currencyTo-1]);
+
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+
+        Request request = new Request.Builder()
+                .url("https://api.apilayer.com/fixer/convert?to="+currency[currencyTo-1]+"&from="+currency[currencyFrom-1]+"&amount="+amount)
+                .addHeader("apikey", "0hzrV7LfPQKIE617SztsBBX9C4ERUM8i")
+                .method("GET",null)
+                .build();
+        Response response = client.newCall(request).execute();
+        System.out.println(response.body().string());
+    }
+}
